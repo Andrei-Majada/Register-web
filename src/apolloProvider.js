c3933a10e354ca301f1a969633c8bc61008f6b31
Andrei Schneider Majada <andrei.majada@softdesign.com.br>
2021-12-27 12:36:18 -0300

    add graphql dependency

--- src/apolloProvider.js
+++ src/apolloProvider.js
@@ -5,7 +5,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { ApolloProvider } from "@apollo/react-hooks";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/api",
+  uri: "http://localhost:4000/api", //utilizando a porta 4000 na api
 });
 
 
